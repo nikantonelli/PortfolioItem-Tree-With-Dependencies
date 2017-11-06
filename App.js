2a3f6fd9ffeac8b013e111a18287652071d354a9
nikantonelli <nik@nikantonelli.com>
2017-11-06 12:58:42 +0000

    Update help text

--- App.js
+++ App.js
@@ -1099,17 +1099,23 @@ Ext.define('Rally.apps.PortfolioItemTree.app', {
                             xtype: 'component',
                             html: 
                                 '<p class="boldText">Hierarchical Tree View</p>' +
-                                '<p>This app will find all the children of a particular Portfolio artefact. You can choose the type of artefact, then the artefact itself</p>' +
-                                '<p>The colours of the circles indicate the state of progress from red (those that are not started), through to blue (in their final stages).' +
-                                ' Click on the "Colour Codes" button to see the colour to state mapping for each portfolio item type.</p>' +
+                                '<p>This app will find all the children of a particular Portfolio artefact. You can choose the type of artefact,' +
+                                ' then the top level artefact itself.</p>' +
+                                '<p>The colours of the circles indicate the state of progress from red (those that are not started), through to' +
+                                ' blue (in their final stages). Click on the "Colour Codes" button to see the colour to state mapping for each' +
+                                ' portfolio item type.</p>' +
+                                '<p class="boldText">Choosing collections</p>' +
+                                '<p>The app settings contains an option to allow you to multi-select the top level artefacts. This allows you to' +
+                                ' choose a number of portfolio items of interest and then filter for the features</p>' +
                                 '<p class="boldText">Visualising Dependencies</p>' +
                                 '<p>The edge of the circle will be red if there are any dependencies (predecessors or successors) and the colour ' +
                                 'of the associated text will indicate those with predecessors (red text) and those with successors (green text). ' +
                                 'Those with both will appear as having predecessors</p>' +
                                 '<p>If the text is blinking, it means that the relevant dependency is not being shown in this data set. </p>' +
                                 '<p class="boldText">Exploring the data</p><p>You can investigate dependencies by using &lt;shift&gt;-Click ' +
-                                'on the circle. This will call up an overlay with the relevant dependencies. Clicking on the FormattedID on any artefact will take you to it in' +
-                                ' either the EDP or QDP page (whichever you have enabled for your session)</p>' +
+                                'on the circle. This will call up an overlay with the relevant dependencies. Clicking on the FormattedID on any' +
+                                ' artefact in the overlay will take you to it in either the EDP or QDP page (whichever you have enabled for your' +
+                                ' session )</p>' +
                                 '<p>If you click on the circle without using shift, then a data panel will appear containing more information about that artefact</p>' +
                                 '<p class="boldText">Filtering</p>' +
                                 '<p>There are app settings to enable the extra filtering capabilities on the main page, so that you can choose which lowest-level portfolio items to see' +
